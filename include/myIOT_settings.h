f3c45027a40a38f6e77bc7aaffb19206f5e59b13
guydv <guydvir.tech@gmail.com>
2024-11-13 14:18:42 +0200

    ;

--- include/myIOT_settings.h
+++ include/myIOT_settings.h
@@ -1,54 +1,80 @@
 myIOT2 iot;
+
 extern void set_disarm();
-extern uint8_t get_systemState();
-extern void set_armState(uint8_t req_state);
+extern uint8_t get_system_stateCode();
+extern bool set_arm_withCode(uint8_t _armType);
+extern uint8_t atBoot_systemState;
 
-void pub_systemState(uint8_t state = 5)
+void pub_stateTopic(uint8_t state)
 {
-    if (state == 5) /* Not having any valid / input*/
-    {
-        state = get_systemState();
-    }
-
     iot.pub_state(sys_states[state]);
+}
+void pub_AlarmSystemState(uint8_t state)
+{
+    char a[50];
+    // char b[30];
+    // const char *origins[2] = {"Controller", "KeyPad"};
+
+    sprintf(a, "SystemState: [%s]", sys_states[state]);
+    iot.pub_msg(a);
+
+    // if (state == ARMED_AWAY_CODE || state == ARMED_HOME_CODE)
+    // {
+    //     sprintf(b, " using [%s]", origins[0]);
+    // }
+    // else if (state == DISARMED || state == ALARMING)
+    // {
+    //     sprintf(b, " ");
+    // }
+    // else if (state == ARMED_KEYPAD)
+    // {
+    //     sprintf(b, " using [%s]", origins[1]);
+    // }
+    // else
+    // {
+    //     iot.pub_msg("Err");
+    // }
+    // sprintf(a, "SystemState: from[%s] to [%s]%s",
+    //         sys_states[atBoot_systemState], sys_states[state], b);
+    // iot.pub_msg(a);
+}
+void pub_ControllerCMD(uint8_t state)
+{
+    char a[50];
+    sprintf(a, "ControllerCMD: [%s]", sys_states[state]);
+    iot.pub_msg(a);
 }
 void setMQTTTopics()
 {
-  const char *t[] = {"DvirHome/Messages", "DvirHome/log", "DvirHome/debug"};
-  const char *t2[] = {"DvirHome/alarmMonitor", "DvirHome/All"};
-  const char *t3[2] = {"DvirHome/alarmMonitor/Avail", "DvirHome/alarmMonitor/State"};
+    const char *t[] = {"DvirHome/Messages", "DvirHome/log", "DvirHome/debug"};
+    const char *t2[] = {"DvirHome/alarmMonitor", "DvirHome/All"};
+    const char *t3[2] = {"DvirHome/alarmMonitor/Avail", "DvirHome/alarmMonitor/State"};
 
-  iot.add_gen_pubTopic(t, sizeof(t) / sizeof(t[0]));
-  iot.add_subTopic(t2, sizeof(t2) / sizeof(t2[0]));
-  iot.add_pubTopic(t3, sizeof(t3) / sizeof(t3[0]));
+    iot.add_gen_pubTopic(t, sizeof(t) / sizeof(t[0]));
+    iot.add_subTopic(t2, sizeof(t2) / sizeof(t2[0]));
+    iot.add_pubTopic(t3, sizeof(t3) / sizeof(t3[0]));
 }
-void update_Parameters()
-{
-    iot.useSerial = false;
-    iot.useFlashP = false;
-    iot.noNetwork_reset = 15;
-    iot.ignore_boot_msg = false;
-}
+
 void addiotnalMQTT(char *incoming_msg, char *_topic)
 {
     char msg[100];
 
     if (strcmp(incoming_msg, "status") == 0)
     {
-        sprintf(msg, "Status: [%s]", sys_states[get_systemState()]);
+        sprintf(msg, "Status: [%s]", sys_states[get_system_stateCode()]);
         iot.pub_msg(msg);
     }
     else if (strcmp(incoming_msg, sys_states[ARMED_HOME_CODE]) == 0) /* arm Home */
     {
-        set_armState(ARMED_HOME_CODE);
+        set_arm_withCode(ARMED_HOME_CODE);
     }
     else if (strcmp(incoming_msg, sys_states[ARMED_AWAY_CODE]) == 0) /* arm Away */
     {
-        set_armState(ARMED_AWAY_CODE);
+        set_arm_withCode(ARMED_AWAY_CODE);
     }
     else if (strcmp(incoming_msg, sys_states[DISARMED]) == 0) /* disarmed */
     {
-        set_armState(DISARMED);
+        set_arm_withCode(DISARMED);
     }
     else if (strcmp(incoming_msg, "ver2") == 0)
     {
@@ -57,14 +83,8 @@ void addiotnalMQTT(char *incoming_msg, char *_topic)
     }
     else if (strcmp(incoming_msg, "clear") == 0)
     {
-        digitalWrite(SET_SYSTEM_ARMED_HOME_PIN, STATE_ON);
-        delay(DELAY_TO_REACT);
-        digitalWrite(SET_SYSTEM_ARMED_HOME_PIN, !STATE_ON);
-        delay(DELAY_TO_REACT);
-
-        digitalWrite(SET_SYSTEM_ARMED_AWAY_PIN, STATE_ON);
-        delay(DELAY_TO_REACT);
-        digitalWrite(SET_SYSTEM_ARMED_AWAY_PIN, !STATE_ON);
+        digitalWrite(CONTROLLER_ARM_HOME_PIN, !STATE_ON);
+        digitalWrite(CONTROLLER_ARM_AWAY_PIN, !STATE_ON);
         delay(DELAY_TO_REACT);
 
         iot.sendReset("Reset via MQTT");
@@ -84,17 +104,24 @@ void addiotnalMQTT(char *incoming_msg, char *_topic)
     }
     else if (strcmp(incoming_msg, "debug") == 0)
     {
-        sprintf(msg, "armPin is [%s], AlarmPin is [%s], ArmHome is [%s], ArmAway is [%s]",
-                digitalRead(SYSTEM_STATE_ARM_PIN) ? "OFF" : "ON",
-                digitalRead(SYSTEM_STATE_ALARM_PIN) ? "OFF" : "ON",
-                digitalRead(SET_SYSTEM_ARMED_HOME_PIN) ? "ON" : "OFF",
-                digitalRead(SET_SYSTEM_ARMED_AWAY_PIN) ? "ON" : "OFF");
+        sprintf(msg, "Armed_Home [%s], Armed_Away is [%s], armPin [%s], AlarmPin [%s]",
+                controllerState.is_armed_home ? "ON" : "OFF",
+                controllerState.is_armed_away ? "ON" : "OFF",
+                alarmsysState.is_armed ? "ON" : "OFF",
+                alarmsysState.is_alarming ? "ON" : "OFF");
         iot.pub_msg(msg);
     }
+}
+void def_iot_parameters()
+{
+    iot.useSerial = false;
+    iot.useFlashP = false;
+    iot.noNetwork_reset = 15;
+    iot.ignore_boot_msg = false;
 }
 void startIOTservices()
 {
     setMQTTTopics();
-    update_Parameters();
+    def_iot_parameters();
     iot.start_services(addiotnalMQTT);
 }
--- include/systemDefs.h
+++ include/systemDefs.h
@@ -1,24 +1,39 @@
 // ~~~~ HW Pins and States ~~~~
-#define DELAY_TO_REACT 3000 // millis
+#define DELAY_TO_REACT 1500 // millis
 #define STATE_ON HIGH
 #define SYSTEM_STATE_ON LOW
-#define SYSTEM_STATE_ARM_PIN D3      //  Indication system is Armed
-#define SYSTEM_STATE_ALARM_PIN D4    //  Indication system is Alarmed
-#define SET_SYSTEM_ARMED_HOME_PIN D5 //   (Set system)  armed_Home
-#define SET_SYSTEM_ARMED_AWAY_PIN D6 //   (Set system)  Armed_Away
+#define SYSTEM_STATE_ARM_PIN D1    //  Indication system is Armed
+#define SYSTEM_STATE_ALARM_PIN D2  //  Indication system is Alarmed
+#define CONTROLLER_ARM_HOME_PIN D5 //   (Set system)  armed_Home
+#define CONTROLLER_ARM_AWAY_PIN D6 //   (Set system)  Armed_Away
 
-const char *VER = "alarmMon_4.1";
+const uint8_t deBounceInt = 50;
+
+const char *VER = "alarmMon_5.0";
+struct AlarmSystemState
+{
+    bool is_armed = false;
+    bool is_alarming = false;
+} alarmsysState;
+
+struct ContollertState
+{
+    bool is_armed_home = false;
+    bool is_armed_away = false;
+} controllerState;
+
 const char *sys_states[5] = {
+    "disarmed",
     "armed_home",
     "armed_away",
-    "disarmed",
     "pending",
     "triggered"};
+
 enum sys_state : const uint8_t
 {
+    DISARMED,
     ARMED_HOME_CODE,
     ARMED_AWAY_CODE,
-    DISARMED,
     ARMED_KEYPAD,
     ALARMING,
     ARM_ERR,
